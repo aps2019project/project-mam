136299cb0cd3a8455b6ff473ba9bab6f68e03a9c
jamshidi799 <jamshidi.m799@gmail.com>
2019-04-12 18:55:47 +0430

    bug Fix 0.00002

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -7,14 +7,16 @@ import Model.*;
 public class Controller {
 
     private static final Controller CONTROLLER = new Controller();
-    private Controller(){}
 
-    static Controller getInstance(){
+    private Controller() {
+    }
+
+    static Controller getInstance() {
         return CONTROLLER;
     }
+
     private View view = View.getInstance();
     User user = new User();
-
 
 
 //------------------------------user account -----------------------------
@@ -27,10 +29,11 @@ public class Controller {
         System.out.println("password :");
         request.getNewCommand();
         String password = request.getCommand();
-        if(User.isUserNameNew(userName))
+        if (User.isUserNameNew(userName))
             User.addUser(new User(userName, password));
     }
-    void loginAccount(Request request){
+
+    void loginAccount(Request request) {
         String userName = request.getCommand();
         System.out.println("password :");
         request.getNewCommand();
@@ -40,46 +43,65 @@ public class Controller {
             user = temp;
     }
 
-    void logoutAccount(Request request){
+    void logoutAccount(Request request) {
         user = null;
     }
 
-    void showLeaderBoard(Request request){}
+    void showLeaderBoard(Request request) {
+    }
     //--------------------------------------collection------------------------------
 
-    void isAccountValidForLogin(Request request){}
+    boolean isAccountValidForLogin(Request request){
+        return true;
+    }
 
-    void showCollection(Request request){}
+    void showCollection(Request request) {
+    }
 
-    void searchInCollection(Request request){}
+    void searchInCollection(Request request) {
+    }
 
-    void saveCommandInCollection(){}
+    void saveCommandInCollection() {
+    }
 
-    void createDeck(Request request){}
+    void createDeck(Request request) {
+    }
 
-    void deleteDeck(Request request){}
+    void deleteDeck(Request request) {
+    }
 
-    void addCardToDeck(Request request){}               //???????
+    void addCardToDeck(Request request) {
+    }               //???????
 
-    void removeFromDeck(Request request){}
+    void removeFromDeck(Request request) {
+    }
 
-    void isDeckValid(Request request){}
+    void isDeckValid(Request request) {
+    }
 
-    void selectMainDeck(Request request){}
+    void selectMainDeck(Request request) {
+    }
 
-    void showDeck(){}
+    void showDeck() {
+    }
 
-    void showDeck(Request request){}
+    void showDeck(Request request) {
+    }
     //------------------------------------------Shop-----------------------
 
-    void searchInShop(Request request){}
+    void searchInShop(Request request) {
+    }
 
-    void showCollectionInShop(Request request){}
+    void showCollectionInShop(Request request) {
+    }
 
-    void buyCard(Request request){}
+    void buyCard(Request request) {
+    }
 
-    void sellCard(Request request){}
+    void sellCard(Request request) {
+    }
 
-    void showShop(){}
+    void showShop() {
+    }
 
 }
