48d6d423b5c628a8b8a3016e4c6460be96e81820
alijavanmard <ali.javanmard14@gmail.com>
2019-05-04 17:24:40 +0430

    make Ai1

--- src/Model/AI.java
+++ src/Model/AI.java
@@ -4,6 +4,8 @@ import Controller.Controller;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
+
 import static java.lang.Math.abs;
 
 public class AI {
@@ -11,6 +13,7 @@ public class AI {
 
     private Game game;// = Controller.getInstance().getGame();
     private static final User ai = new User("AI", "0");
+    private int counter;
 
     public static User getAI() {
         return ai;
@@ -23,21 +26,25 @@ public class AI {
     private Cell moveTarget = null;
 
     public String getCommand() {
+        if (cardsCanMove())
+            moveCards();
+
         return command;
     }
 
-    public void moveCard(int x, int y){
+    public void moveCard(int x, int y) {
         StringBuilder newCommand = new StringBuilder();
         newCommand.append("move to ").append(x).append(" ").append(y);
         command = newCommand.toString();
+        counter += 1;
     }
 
-    public boolean cardCanMove(Card card){
+    public boolean cardCanMove(Card card) {
         for (Map.Entry<Integer, Cell> entry : game.getMap().getSecondPlayerCellCard().entrySet()) {
-            if (entry.getValue().getCard().getName().equalsIgnoreCase(card.getName())){
+            if (entry.getValue().getCard().getName().equalsIgnoreCase(card.getName())) {
                 for (Cell[] cells : game.getMap().getCells()) {
                     for (Cell cell : cells) {
-                        if (game.getMap().getManhatanDistance(cell, entry.getValue()) < entry.getValue().getCard().getTargetCommunity()){
+                        if (game.getMap().getManhatanDistance(cell, entry.getValue()) < entry.getValue().getCard().getTargetCommunity()) {
                             moveTarget = cell;
                             return true;
                         }
@@ -48,15 +55,25 @@ public class AI {
         return false;
     }
 
-    public void moveCards(){
+    public boolean cardsCanMove() {
+        for (Map.Entry<Integer, Cell> entry : game.getMap().getSecondPlayerCellCard().entrySet()) {
+            if (entry.getValue().getCard().isCanMove()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void moveCards() {
         for (Map.Entry<Integer, Cell> entry : game.getMap().getSecondPlayerCellCard().entrySet()) {
             if (cardCanMove(entry.getValue().getCard())) {
                 moveCard(moveTarget.getColumn(), moveTarget.getRow());
+                break;
             }
         }
     }
 
-    public void selectCard(){
+    public void selectCard() {
 
     }
 
