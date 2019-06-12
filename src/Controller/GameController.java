decc0db96f3196b483005dd0f3abd75101a176b4
mahdifarzadi <mahdifarzadi78@gmail.com>
2019-06-13 01:31:11 +0430

    game.fxml id's in game controller initialized

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,4 +1,92 @@
 package Controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+
 public class GameController {
+
+    public Label firstPlayerName;
+    public Label firstPlayerMana;
+    public Label secondPlayerName;
+    public Label secondPlayerMana;
+    public Label item1mana;
+    public Label item2mana;
+    public Label handCard1mana;
+    public Label handCard2mana;
+    public Label handCard3mana;
+    public Label handCard4mana;
+    public Label handCard5mana;
+
+
+    public ImageView cell00;
+    public ImageView cell10;
+    public ImageView cell20;
+    public ImageView cell30;
+    public ImageView cell40;
+    public ImageView cell50;
+    public ImageView cell60;
+    public ImageView cell70;
+    public ImageView cell80;
+
+    public ImageView cell01;
+    public ImageView cell11;
+    public ImageView cell21;
+    public ImageView cell31;
+    public ImageView cell41;
+    public ImageView cell51;
+    public ImageView cell61;
+    public ImageView cell71;
+    public ImageView cell81;
+
+    public ImageView cell02;
+    public ImageView cell12;
+    public ImageView cell22;
+    public ImageView cell32;
+    public ImageView cell42;
+    public ImageView cell52;
+    public ImageView cell62;
+    public ImageView cell72;
+    public ImageView cell82;
+
+    public ImageView cell03;
+    public ImageView cell13;
+    public ImageView cell23;
+    public ImageView cell33;
+    public ImageView cell43;
+    public ImageView cell53;
+    public ImageView cell63;
+    public ImageView cell73;
+    public ImageView cell83;
+
+    public ImageView cell04;
+    public ImageView cell14;
+    public ImageView cell24;
+    public ImageView cell34;
+    public ImageView cell44;
+    public ImageView cell54;
+    public ImageView cell64;
+    public ImageView cell74;
+    public ImageView cell84;
+
+
+
+
+
+
+
+    public ImageView graveyard;
+    public ImageView exit;
+    public ImageView help;
+    public ImageView endTurn;
+
+    public Circle item1;
+    public Circle item2;
+    public Circle nextCard;
+    public Circle handCard1;
+    public Circle handCard2;
+    public Circle handCard3;
+    public Circle handCard4;
+    public Circle handCard5;
 }
